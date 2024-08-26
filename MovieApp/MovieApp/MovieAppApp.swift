f5b70d22d68f18b6ee1c6e2e45c6dbafc0785037
YaNeGleb <zabrodagleb77@gmail.com>
2024-08-26 14:33:44 +0300

    initial commit

---
+++ MovieApp/MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Zabroda Gleb on 26.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
